05042f95dbe8ee9340ea879b94e53d1d1568615c
Your Name <bahiy14@gmail.com>
2024-10-31 03:12:58 -0300

    Add another details in home container

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="product-card">
+    <div class="d-flex flex-row w-100 align-items-center">
+      <div class="flex-grow-1">
+        <div class="product-card-content flex-grow-1">
+          <div class="product-card-data-layout">
+            <div class="logo">
+              <img :src="logoSrc" :alt="logoAlt" />
+            </div>
+            <div class="title">{{ title }}</div>
+            <div class="content">
+              <ul class="p text-secondary pl-4">
+                <li v-for="(feature, index) in features" :key="index">
+                  <span class="p-bold">{{ feature.title }}</span>
+                  {{ feature.description }}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="product-card-img-container">
+            <img :src="imageSrc" :alt="imageAlt" width="200px" />
+          </div>
+        </div>
+        <div class="product-card-actions mt-3">
+          <span class="price">{{ price }}</span>
+          <button type="button" class="btn-custom" @click="addToPlan">
+            <i class="fa fa-plus"></i> {{ buttonText }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    logoSrc: { type: String, required: true },
+    logoAlt: { type: String, default: "product-logo" },
+    title: { type: String, required: true },
+    features: {
+      type: Array,
+      required: true,
+      default: () => [
+        { title: "Feature Title", description: "Feature description" },
+      ],
+    },
+    imageSrc: { type: String, required: true },
+    imageAlt: { type: String, default: "" },
+    price: { type: String, required: true },
+    buttonText: { type: String, default: "Adicionar ao plano" },
+  },
+  methods: {
+    addToPlan() {
+      // Aqui você pode adicionar lógica para adicionar ao plano ou chamar uma função externa
+      this.$emit("add-to-plan");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.product-card {
+  margin-bottom: 3rem !important;
+  display: flex;
+  padding: 3rem !important;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 3rem;
+  align-self: stretch;
+  border-radius: 1rem;
+  border: 1px solid var(--stroke-heavy-contrast, #cbd5e1);
+  background: var(--surface-primary, #fff);
+
+  .product-card-data-layout {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    height: min-content;
+
+    .title {
+      font-weight: bold;
+    }
+
+    .content {
+      padding: 10px 15px 15px 3rem !important;
+    }
+  }
+
+  .product-card-content {
+    display: flex;
+    justify-content: space-between !important;
+  }
+
+}
+.product-card-actions {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 1rem;
+}
+.price {
+  font-weight: 600;
+  font-size: 24px;
+}
+</style>
