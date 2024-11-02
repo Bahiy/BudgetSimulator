a14cef57c94d32cf6a0927fe099e8d2e29de9375
Your Name <bahiy14@gmail.com>
2024-11-02 01:59:14 -0300

    Add pinia for global storage

--- src/main.js
+++ src/main.js
@@ -4,13 +4,25 @@ import router from "./router";
 import "./assets/scss/app.scss";
 import money from "v-money";
 import "./utils/directives";
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/default.css";
+import {createPinia} from "pinia";
 
-Vue.use(money, {
-  decimal: ",",
-  thousands: ".",
-  prefix: "R$ ",
-  precision: 2,
-});
+const pinia = createPinia();
+
+Vue.use(pinia);
+
+Vue.component("VueSlider", VueSlider);
+Vue.use(
+  money,
+  {
+    decimal: ",",
+    thousands: ".",
+    prefix: "R$ ",
+    precision: 2,
+  },
+  pinia
+);
 Vue.config.productionTip = false;
 
 new Vue({
